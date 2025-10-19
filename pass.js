a7e1ef25c8368d600bd9b92cda4d1b26e2c358e5
Nibhintk <nibhintk541@gmail.com>
2025-10-19 20:02:06 +0530

    Updated pass.js with latest changes

--- pass.js
+++ pass.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 
 async function hashPassword() {
-  const password = "admin123"; // your admin password
+  const password = process.env.ADMIN_PASS; // your admin password
   const hashed = await bcrypt.hash(password, 10);
   console.log(hashed);
 }
